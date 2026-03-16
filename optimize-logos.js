90f0bf0847a6ebf43e6b08254243090f520bfdd8
grospitz-wreck-it <grospitz@gmail.com>
2026-03-16 13:57:16 +0100

    Update optimize-logos.js

--- optimize-logos.js
+++ optimize-logos.js
@@ -1,4 +1,3 @@
-console.log("Starte Logo-Optimierung...")
 import fs from "fs"
 import sharp from "sharp"
 
@@ -6,35 +5,30 @@ const folder = "./logos"
 
 async function optimize(){
 
+console.log("Starte Logo-Optimierung...")
+
 const files = fs.readdirSync(folder)
-console.log(files)
+
 for(const file of files){
 
 if(!file.endsWith(".png") && !file.endsWith(".jpg")) continue
 
 const input = `${folder}/${file}`
 
+const output = `${folder}/opt-${file}`
+
 try{
 
 await sharp(input)
-.resize({
-width:40,
-height:40,
-fit:"inside"
-})
-.png({
-compressionLevel:9,
-quality:80
-})
-.toFile(`${input}.tmp`)
+.resize(40,40,{fit:"contain"})
+.png({compressionLevel:9})
+.toFile(output)
 
-fs.renameSync(`${input}.tmp`,input)
-
-console.log("✔ optimiert:",file)
+console.log("✔ erstellt:",output)
 
 }catch(err){
 
-console.log("⚠ Fehler:",file)
+console.log("Fehler bei:",file)
 
 }
 
