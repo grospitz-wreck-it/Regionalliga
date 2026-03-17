7789f222eb45ef5217591a612bc964de1d9543f0
grospitz-wreck-it <grospitz@gmail.com>
2026-03-17 15:04:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,33 @@
 async function loadTable(){
 
-const res = await fetch("table.json");
-const data = await res.json();
+const res = await fetch("table.json")
+const data = await res.json()
 
-const tbody = document.getElementById("table-body");
+const tbody = document.getElementById("table-body")
+tbody.innerHTML = ""
 
-tbody.innerHTML="";
+data.forEach(team => {
 
-data.forEach((t,index)=>{
+const row = document.createElement("tr")
 
-const row=document.createElement("tr");
-
-if(t.position===1){
-row.classList.add("promoted");
-}
-
-if(index>=data.length-3){
-row.classList.add("relegated");
-}
-
-row.innerHTML=`
-<td>${t.position}</td>
+row.innerHTML = `
+<td>${team.position}</td>
 <td class="team">
-<img src="${t.logo}">
-${t.team}
+  <img src="${team.logo}" onerror="this.style.display='none'">
+  ${team.team}
 </td>
-<td>${t.games}</td>
-<td>${t.wins}</td>
-<td>${t.draws}</td>
-<td>${t.losses}</td>
-<td>${t.goals}</td>
-<td>${t.points}</td>
-`;
+<td>${team.games}</td>
+<td>${team.wins}</td>
+<td>${team.draws}</td>
+<td>${team.losses}</td>
+<td>${team.goals}</td>
+<td>${team.points}</td>
+`
 
-tbody.appendChild(row);
+tbody.appendChild(row)
 
-});
+})
 
 }
 
-loadTable();
+loadTable()
